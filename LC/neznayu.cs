835270b7449b1aba7a7812b25349d0519b1da6f5
tacobels819 <tacobels@yandex.ru>
2022-06-14 21:44:13 +0300

    Sock

---
+++ LC/neznayu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LC
+{
+    public class neznayu
+    {
+        public static int Avtoriz(string login, string pasword)
+        {
+            if (login == "admin")
+            {
+                if (pasword == "admin") return 1;
+                else return 2; 
+            }
+            return 0;
+        }
+    }
+}
---
+++ Podgotov/Pages/Avtorizaciya.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LC;
+
+namespace Podgotov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Avtorizaciya.xaml
+    /// </summary>
+    public partial class Avtorizaciya : Page
+    {
+        public Avtorizaciya()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var login = LoginBox.Text;
+            var pasword = Paswordbox.Password;
+            var result = neznayu.Avtoriz (login, pasword);
+            if (result == 1) NavigationService.Navigate(new Uri("Pages/Page1.xaml", UriKind.Relative));
+            else if (result == 2) MessageBox.Show("PaswordnoVerniy");
+            else MessageBox.Show("net");
+
+        }
+    }
+}
